9ca621e9c6c167e616690fe6125c8c6664a0eee8
Nathaniel <nathaniel.jaden@student.moringaschool.com>
2024-05-23 09:44:57 +0300

    Anagram Detector Lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,6 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.sorted_word = sorted(word)
+
+    def match(self, word_list):
+        return [word for word in word_list if sorted(word) == self.sorted_word]
